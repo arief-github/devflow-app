4518f81a3191d0ea68e4e19c2ee6392f837343a8
arief-github <ariefkurniawan0411@gmail.com>
2026-05-15 23:49:48 +0700

    feat: integrating clerk for auth feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,12 +43,14 @@ export default function RootLayout({
         <ClerkProvider>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
+              <>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </>
             </Show>
             <Show when="signed-in">
               <UserButton />
