5fdf73162581556f0251a488285f5c72922dde03
arief-github <ariefkurniawan0411@gmail.com>
2026-05-27 23:58:14 +0700

    feat: implement tag input functionality with badge support and removal logic in QuestionForm

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -2,9 +2,9 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { useForm } from "react-hook-form";
+import { ControllerRenderProps, useForm } from "react-hook-form";
 import { QuestionformSchema } from "@/lib/validation";
-
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -18,6 +18,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Editor } from "@tinymce/tinymce-react";
 import { useRef } from "react";
+import { Badge } from "../ui/badge";
 
 const QuestionForm = () => {
   const editorRef = useRef(null);
@@ -34,6 +35,46 @@ const QuestionForm = () => {
   function onSubmit(values: z.infer<typeof QuestionformSchema>) {
     console.log(values);
   }
+
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: ControllerRenderProps<z.infer<typeof QuestionformSchema>>,
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters.",
+          });
+        }
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handleTagRemove = (
+    tag: string,
+    field: ControllerRenderProps<z.infer<typeof QuestionformSchema>>,
+  ) => {
+    if (Array.isArray(field.value)) {
+      const newTags = field.value.filter((t: string) => t !== tag);
+
+      form.setValue("tags", newTags);
+    }
+  };
 
   return (
     <div>
@@ -133,11 +174,33 @@ const QuestionForm = () => {
                   Tags <span className="text-red-500">*</span>
                 </FormLabel>
                 <FormControl className="mt-3.5">
-                  <Input
-                    className="no-focus paragraph-regular background-light900_dark300! light-border-2 text-dark300_light700 min-h-[56px] border"
-                    placeholder="Add Tags..."
-                    {...field}
-                  />
+                  <>
+                    <Input
+                      className="no-focus paragraph-regular background-light900_dark300! light-border-2 text-dark300_light700 min-h-[56px] border"
+                      placeholder="Add Tags..."
+                      onKeyDown={(e) => handleInputKeyDown(e, field)}
+                    />
+                    {field.value.length > 0 && (
+                      <div className="flex-start mt-2.5 gap-2.5">
+                        {field.value.map((tag: string) => (
+                          <Badge
+                            key={tag}
+                            onClick={() => handleTagRemove(tag, field)}
+                            className="subtle-medium background-light800_dark300 text-light400_dark500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          >
+                            {tag}
+                            <Image
+                              src="/icons/close.svg"
+                              alt="Close"
+                              width={12}
+                              height={12}
+                              className="cursor-pointer"
+                            />
+                          </Badge>
+                        ))}
+                      </div>
+                    )}
+                  </>
                 </FormControl>
                 <FormDescription className="body-regular mt-2.5 text-light-500">
                   Add up to 3 tags to describe what your question is about. You
