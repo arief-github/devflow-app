7b7cc00c30cede468bf97d11247a6ee668fac01e
arief-github <ariefkurniawan0411@gmail.com>
2026-06-06 21:36:26 +0700

    feat(auth): integrate user authentication in AskQuestion page and enhance route matching in proxy

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,16 +1,14 @@
 import QuestionForm from "@/components/forms/QuestionForm";
 import { getUserById } from "@/lib/actions/user.action";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 const AskQuestionpage = async () => {
-  // mock user id
-  const userId = "clerk_user_1";
+  const authObject = await auth();
 
-  if (!userId) redirect("/sign-in");
+  if (!authObject.userId) redirect("/sign-in");
 
-  const mongoUser = await getUserById({ userId });
-
-  console.log("mongo user => ", mongoUser);
+  const mongoUser = await getUserById({ userId: authObject.userId });
 
   return (
     <div>
