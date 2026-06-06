f092262fc8b0d76941d32f7a4367d597f3dcc46d
arief-github <ariefkurniawan0411@gmail.com>
2026-06-06 20:04:03 +0700

    feat(webhook): implement webhook handling for user events

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "../types/sharedtypes";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 type GetUserByIdParams = {
   userId: string;
@@ -21,6 +28,59 @@ export async function getUserById(params: GetUserByIdParams) {
     return user;
   } catch (error) {
     console.error("Error fetching user:", error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.error("Error updating user:", error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    const deleteUser = await User.findOneAndDelete({ clerkId });
+
+    return deleteUser;
+  } catch (error) {
+    console.error("Error deleting user:", error);
     throw error;
   }
 }
