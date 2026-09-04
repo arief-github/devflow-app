a7cac9ea09b1f180fb8331d381895ba73148a4c4
arief-github <ariefkurniawan0411@gmail.com>
2026-09-05 01:03:05 +0700

    feat: implement filter functionality for questions retrieval and UI

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -2,16 +2,45 @@
 
 import { HomePageFilters } from "@/constants/filter";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
+import { useState } from "react";
 
 const HomeFilters = () => {
-  const active = "frequent";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 hidden md:flex flex-wrap gap-3">
       {HomePageFilters.map((filter) => (
         <Button
           key={filter.value}
-          onClick={() => {}}
+          onClickCapture={() => handleTypeClick(filter.value)}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === filter.value ? "bg-primary-100 text-primary-500" : "bg-light-800 text-light-500"}`}
         >
           {filter.name}
--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
 import {
   Select,
   SelectContent,
@@ -8,6 +9,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
+import { formUrlQuery } from "@/lib/utils";
 
 interface Props {
   filters: {
@@ -19,15 +21,32 @@ interface Props {
 }
 
 const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const paramFilter = searchParams.get("filter");
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "filter",
+      value,
+    });
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
-    <div className={` relative ${containerClasses}`}>
-      <Select>
+    <div className={`relative ${containerClasses}`}>
+      <Select
+        onValueChange={handleUpdateParams}
+        defaultValue={paramFilter || undefined}
+      >
         <SelectTrigger
           className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
         >
-          <div className="line-clamp-1 flex-1 text-left">
+          <span className="line-clamp-1 flex-1 text-left">
             <SelectValue placeholder="Select a Filter" />
-          </div>
+          </span>
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
